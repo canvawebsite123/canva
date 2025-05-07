af1ba919fca7a29b4f4f1596f7284836ab779106
canva <canvawebsite0@gmail.com>
2025-05-07 19:27:59 +0000

    new canva website

--- convex/templates.js
+++
@@ -1,9 +0,0 @@
-import { query } from "./_generated/server";
-
-export const GetAllTemplatest = query({
-    args: {},
-    handler: async (ctx, args) => {
-        const result = await ctx.db.query('templates').collect();
-        return result;
-    }
-})
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com', 'ik.imagekit.io', 'cdn-icons-png.flaticon.com']
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery"
+      },
+    ],
+  },
 };
 
 export default nextConfig;
